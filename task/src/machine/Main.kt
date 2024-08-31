442353624b3126001db5fff30057e8ee15a8d111
Javier Bertos <javier.bertos@gmail.com>
2024-08-31 02:39:49 +0200

    Stage 2/6 Imngredient calculator

--- task/src/machine/Main.kt
+++ task/src/machine/Main.kt
@@ -1,15 +1,31 @@
 package machine
+import java.util.Scanner
+
+fun getCoffeesAmount(): Int {
+    val reader = Scanner(System.`in`)
+    var coffees: Int?
+
+    do {
+        println("Write how many cups of coffee you will need:")
+        coffees = reader.nextLine()?.toIntOrNull()
+    } while (coffees == null)
+
+    return coffees
+}
 
 fun main() {
+    val waterPerCoffee = 200
+    val milkPerCoffee = 50
+    val beansPerCoffee = 15
+
+    val coffees = getCoffeesAmount()
+
     println(
         """
-            Starting to make a coffee
-            Grinding coffee beans
-            Boiling water
-            Mixing boiled water with crushed coffee beans
-            Pouring coffee into the cup
-            Pouring some milk into the cup
-            Coffee is ready!
+            For $coffees cups of coffee you will need:
+            ${coffees * waterPerCoffee} ml of water
+            ${coffees * milkPerCoffee} ml of milk
+            ${coffees * beansPerCoffee} g of coffee beans
         """.trimIndent()
     )
 }
